4b3837cc2f3d06fe3c67e45c02caf3e76290ee83
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-10-24 20:26:12 +0200

    Bash

--- terraform/shared/init.sh
+++ terraform/shared/init.sh
@@ -11,8 +11,8 @@ scripts_path="$(cd "$(dirname "$0")" && pwd)"
 
 # Install Terraform 0.12.12 if on Linux
 if [[ $(uname) == *"Linux"* ]] && [[ $(terraform version) != *"v0.12.12"* ]]; then
-    wget --quiet "https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip"
-    unzip terraform_0.12.12_linux_amd64.zip
+    wget --quiet --output-document="terraform.zip" "https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip"
+    unzip terraform.zip
     sudo mv terraform /usr/local/bin/
 fi
 
