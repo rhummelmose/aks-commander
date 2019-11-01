1884899dd94fd6a63c4027a938b7d19cfdd592d9
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-11-01 09:11:23 +0100

    Ensure Azure DevOps extension

--- scripts/variable_test.sh
+++ scripts/variable_test.sh
@@ -35,5 +35,10 @@ fi
 
 new_variable_value=$(echo $deployment_targets | jq --compact-output '. += [{"resource-group": "'$resource_group'", "cluster-name": "'$cluster_name'", "namespace": "'$namespace'"}]')
 
+# Credentials
 export AZURE_DEVOPS_EXT_PAT=$azure_devops_pat
+
+# Ensure the Azure DevOps extension is installed
+az extension add --name azure-devops
+
 az pipelines variable-group variable update --organization "https://dev.azure.com/rhummelmose" --project aks-commander --group-id 3 --name AKSCOMM_DEPLOYMENT_TARGETS --value "$new_variable_value"
